31e06d8bf260bcaa5de7897b86ec96162a32eeb0
ashon <ashon8813@gmail.com>
2025-03-09 21:14:16 +0900

    Fix nav icons

--- client/src/layouts/MainLayout.tsx
+++ client/src/layouts/MainLayout.tsx
@@ -28,31 +28,14 @@ export default function MainLayout() {
 
   return (
     <div>
-      <div className="flex justify-between items-center bg-base-200/70 backdrop-blur-xl px-4 fixed top-0 left-0 right-0 z-50">
+      <div className="p-2 flex justify-between items-center bg-base-200/70 backdrop-blur-xl fixed top-0 left-0 right-0 z-50">
         <div className="flex items-center gap-2">
-          <Link
-            to="/"
-            className="flex items-center gap-2 text-sm px-2 font-bold hover:text-primary transition-colors"
-          >
-            <img src={Logo} className="logo h-6" alt="AgentFleet Logo" />
-            AgentFleet
-          </Link>
-          <ul className="menu menu-horizontal gap-1 hidden sm:flex">
-            <li>
-              <Link to="/fleets">Fleets</Link>
-            </li>
-            <li>
-              <Link to="/agents">Agents</Link>
-            </li>
-            <li>
-              <Link to="/connectors">Connectors</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-          </ul>
           <div className="dropdown dropdown-start sm:hidden">
-            <div tabIndex={0} role="button" className="btn btn btn-ghost">
+            <div
+              tabIndex={0}
+              role="button"
+              className="p-2 btn btn-sm btn-ghost"
+            >
               <Bars3Icon className="h-5 w-5" />
             </div>
             <ul
@@ -68,11 +51,26 @@ export default function MainLayout() {
               <li>
                 <Link to="/connectors">Connectors</Link>
               </li>
-              <li>
-                <Link to="/about">About</Link>
-              </li>
             </ul>
           </div>
+          <Link
+            to="/"
+            className="flex items-center gap-2 text-sm px-2 font-bold hover:text-primary transition-colors"
+          >
+            <img src={Logo} className="logo h-6" alt="AgentFleet Logo" />
+            AgentFleet
+          </Link>
+          <ul className="menu menu-horizontal gap-1 hidden sm:flex p-0">
+            <li>
+              <Link to="/fleets">Fleets</Link>
+            </li>
+            <li>
+              <Link to="/agents">Agents</Link>
+            </li>
+            <li>
+              <Link to="/connectors">Connectors</Link>
+            </li>
+          </ul>
         </div>
         <div className="flex items-center gap-2">
           <button
@@ -88,7 +86,7 @@ export default function MainLayout() {
         </div>
       </div>
 
-      <main className="pt-16 px-4">{<Outlet />}</main>
+      <main className="pt-14 px-4">{<Outlet />}</main>
     </div>
   )
 }
